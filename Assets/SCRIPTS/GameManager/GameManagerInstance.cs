f8949f9fa7e25890d716aa361ccd423b48c0ea97
Nicodragon25 <nicolasariellaure@gmail.com>
2024-08-26 17:56:31 -0300

    Add player prefab

---
+++ Assets/SCRIPTS/GameManager/GameManagerInstance.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerInstance : MonoBehaviour
+{
+    [SerializeField] private GameManagerSO gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
